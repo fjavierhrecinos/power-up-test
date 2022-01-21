b119f8e245a6e7709cd9c3ad25f714ec4e2a1e5f
Francisco Hernandez <fjavier86+github@gmail.com>
2022-01-21 16:34:58 -0600

    feat: add cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
+const express = require('express');
+const cors = require('cors');
 
-var app = express();
+const app = express();
+app.use(cors({ origin: 'https://trello.com' }));
 
 app.use(express.static('public'));
 
@@ -9,6 +11,6 @@ app.get("*", function (request, response) {
 });
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+const listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
